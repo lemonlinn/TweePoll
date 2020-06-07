85e929378fa3dc38e29dc0b2304289e1f1f04692
Lemon <54917401+lemonlinn@users.noreply.github.com>
2020-06-06 23:54:48 -0400

    Misc changes
    Changing directory structure to fit flask and some other odds and ends

--- TweePoll_Server.py
+++ TweePoll_Server.py
@@ -1,2 +1,18 @@
 import TweePoll_API
 from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET','POST'])
+def index():
+	return(render_template("index.html"))
+	
+@app.route('/inputs', methods = ['POST'])
+def inputs():
+	if request.method == "POST":
+		if request.form['variable']:
+			#return variable
+			return(request.form['variable'])
+
+if __name__ == '__main__':
+	app.run(port = 5000, debug = True)
